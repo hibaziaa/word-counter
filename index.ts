31fdf46177ab2eb6abb5ca60c8cd7bf532a34d0d
hibaziaa <hzia10624@gmail.com>
2024-06-15 02:56:08 +0500

    pushing my word counter project to github

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+
+//declaring a constant 'answer', to store user's input.
+const answer: {
+  sentence: string;
+} = await inquirer.prompt([
+  {
+    name: "sentence",
+    type: "input",
+    message: "Enter a paragraph to count words in it:",
+  },
+]);
+
+//storing user's input in a constant variable and applying strings methods that will remove whitespaces in the paragraph and split them in array of single words(strings).
+const words = answer.sentence.trim().split(" ");
+
+//print the word count of array to the console.
+console.log(`Your paragraph's word count is ${words.length}`);
